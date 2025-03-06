61e39d450725a1b3d79a972342516a275b249a69
simoneburrai <burrai.s98@gmail.com>
2025-03-06 15:09:14 +0100

    feat: created two arrays checking values function

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const minNumber = 1;
 const maxNumber = 50;
 // array with casual numbers 
 const casualArray = generateRandomNumbers(minNumber, maxNumber);
-let timerCount = 10;
+// Timer count 
+let timerCount = 3;
 
+arrayProva = [3, 2, 7, 8, 9];
+
+const userNumbers = [];
+
+// HTML Elements 
 const numbersListElement = document.getElementById("numbers-list");
 const countDownElement = document.getElementById("countdown");
 const formElement = document.getElementById("answers-form");
-console.log(formElement);
-console.log(countDownElement)
+const instructionsElement = document.getElementById("instructions");
+const instructions2Element = document.getElementById("instructions2");
+const buttonElement = document.querySelector("button");
 
 // Stampo in console i numeri casuali generati 
 for(i=0; i < casualArray.length; i++){
@@ -19,19 +26,20 @@ for(i=0; i < casualArray.length; i++){
     numbersListElement.append(pageCurrentNumber);
 }
 
+// timer interval function 
+const timerInterval = setInterval(intervalCounter, 1_000);
 
-const timerInterval = setInterval(function(){
-    countDownElement.innerText = timerCount;
-    console.log(timerCount);
-    console.log(countDownElement);
-    timerCount--;
-    if(timerCount === 0){
-        countDownElement.innerText = 0;
-        numbersListElement.classList.replace("d-flex", "d-none");
-        formElement.classList.remove("d-none");
-        clearInterval(timerInterval);
+// Form Event listener 
+formElement.addEventListener("submit", function(event){
+    event.preventDefault();
+    const inputElements = document.querySelectorAll("input");
+    for(i=0; i<inputElements.length; i++){
+        userNumbers.push(parseInt(inputElements[i].value));
     }
-}, 1_000);
+    console.log(userNumbers);
+    console.log(casualArray);
+    console.log(twoArraysValuesControl(userNumbers, casualArray));
+});
 
 
 
@@ -42,13 +50,10 @@ const timerInterval = setInterval(function(){
 
 
 
+// -------------------- FUNCTIONS ---------------------------
 
 
-
-// Functions 
-
-
-
+// Funzione che genera un array con 5 numeri casuali 
 
 function generateRandomNumbers (min, max){
     const arrayNumbers = [];
@@ -61,3 +66,33 @@ function generateRandomNumbers (min, max){
 }
 
 
+// Counter from 10 to 0 interval function 
+function intervalCounter (){
+    countDownElement.innerText = timerCount;
+    timerCount--;
+    if(timerCount === 0){
+        countDownElement.innerText = 0;
+        // all display and not display elements settings 
+        numbersListElement.classList.replace("d-flex", "d-none");
+        formElement.classList.remove("d-none");
+        instructionsElement.classList.add("d-none");
+        instructions2Element.classList.remove("d-none");
+        clearInterval(timerInterval);
+    }
+}
+
+
+
+
+// Funzione per controllare due array se hanno valori uguali e inserirli in un nuovo array
+function twoArraysValuesControl (arr, arr2){
+    const equalValuesArray = [];
+    console.log("Questo è l'array dell'utente", arr);
+    console.log("Questo è l'array casuale", arr2);
+    for(let i=0; i < arr.length; i++){
+        if(arr2.includes(arr[i])){
+            equalValuesArray.push(arr[i]);
+        }
+    }
+    return equalValuesArray;
+}
